c62e0cc8c0fd85c30189578a6d3914b5d10c2efe
armandop.bringas <bringas.armandop@gmail.com>
2020-07-03 23:37:40 -0500

    adding styles to close buton

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -6,13 +6,15 @@ const Modal = ({ children, show, setShow }) => {
   const content = show && (
     <div className="overlay">
       <div className="modal">
-        <button
-          className="modal-close"
-          type="button"
-          onClick={() => setShow(false)}
-        >
-          X
-        </button>
+        <div className="modal-close">
+          <button
+            type="button"
+            className='close-button'
+            onClick={() => setShow(false)}
+          >
+            X
+          </button>
+        </div>
         <div className="modal-body">
             <h1>{children}</h1>
         </div>
