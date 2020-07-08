3f580b63fa27d29cd271d4fcc0392c53a141fda6
armandop.bringas <bringas.armandop@gmail.com>
2020-07-08 10:59:35 -0500

    maybe last commit

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -7,6 +7,9 @@ const Form = () => {
         name: '',
         email: ''
     })
+    const [error, setError] =useState(false)
+
+    const { name, email } = send
 
     const handleInputChange = e => {
         setSend({
@@ -16,19 +19,30 @@ const Form = () => {
     }
 
     const sendData = e => {
-        e.preventDefault()
-        console.log(send.name + ' ' + send.email)
+      e.preventDefault()
+      if(name.trim() === '' || email.trim() === '') {
+        setError(true)
+        return
+      }
+      setError(false)
+      // Reset form
+      setSend({
+        name: '',
+        email: ''
+      })
     }
 
     return (
         <form
             onSubmit={sendData}
+            className='formBody'
         >
               <p>Name</p>
               <input 
                 name='name'
                 className='inputForm1'
-                placeholder='Name'
+                placeholder='name'
+                value={name}
                 onChange={handleInputChange}
               />
               <p>E-mail</p>
@@ -36,14 +50,22 @@ const Form = () => {
                 name='email'
                 className='inputForm2'
                 placeholder='e-mail'
+                value={email}
                 onChange={handleInputChange}
               />
               <button 
-                className='submitButton'
                 type='submit'
+                className='submitButton'
               >
                 Send
               </button>
+              {error ? 
+                <span
+                  className='alertForm'
+                >
+                  Todos los campos son obligatorios
+                </span> 
+              : null}
         </form>
     )
 }
